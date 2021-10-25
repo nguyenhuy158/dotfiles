08e0f02db2334a7a5478de86d924bd9f00c7b3a2
Huy Nguyen <hyquaq@gmail.com>
2021-10-25 14:07:01 +0700

    quick commit

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,12 +1,15 @@
 alias ns="npm start"
 alias nrd="npm run deploy"
 
+alias ..='cd ..'
+alias ...='cd ../..'
 alias cdd="cd -"
 
 alias gp="git push"
 alias gl="git pull"
 alias gqc='git add . && git commit -m "quick commit"'
 alias push-vim="gqc && gp"
+
 
 
 alias move-vim-push="cd ~/vim && push-vim"
