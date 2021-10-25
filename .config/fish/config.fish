87ac34225f64cc23feb346ee6fb0e229c91d392c
Huy Nguyen <hyquaq@gmail.com>
2021-10-25 14:24:07 +0700

    quick commit

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -16,17 +16,17 @@ alias apt="sudo apt"
 alias gp="git push"
 alias gl="git pull"
 alias gqc='git add . && git commit -m "quick commit"'
-alias push-vim="gqc && gp"
+alias push-vim="gqc && code ."
 
 
 
-alias move-vim-push="cd ~/vim && push-vim"
+alias move-vim-code="cd ~/vim && push-vim"
 alias backup-alias="cp ~/.config/fish/config.fish ~/vim/.config/fish/"
 alias backup-vimrc="cp ~/.vimrc ~/vim/"
 alias backup-vimrcplug="cp ~/.vimrc.plug ~/vim/"
 alias backup-coc="cp ~/.vim/coc-settings.json ~/vim/.vim/"
 
 alias bc-vm="backup-alias && backup-vimrc && backup-vimrcplug && backup-coc" 
-alias backup-vim="bc-vm && move-vim-push && cd - "
+alias backup-vim="bc-vm && move-vim-code"
 
 
