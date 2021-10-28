d60105984769c19e93895010fca07ac171516248
Huy Nguyen <hyquaq@gmail.com>
2021-10-29 06:06:11 +0700

    quick commit

---
+++ .config/fish/alias.fish
@@ -0,0 +1,62 @@
+alias vi="bneovim && nvim"
+
+# alias npm  {{{
+
+alias ns="npm start"
+alias ni="npm install"
+alias na="npm audit"
+alias nf="npm fund"
+alias nd="npm run deploy"
+
+#}}}
+
+alias c='clear'
+alias h='history'
+alias .='cd'
+alias ..='cd ..'
+alias ...='cd ../..'
+alias cdd="cd -"
+alias home="~"
+
+alias c='clear'
+alias h='history'
+alias .='cd'
+alias ..='cd ..'
+alias ...='cd ../..'
+alias cdd="cd -"
+alias home="~"
+
+alias mkdir='mkdir -pv'
+
+
+alias apt-get="sudo apt-get"
+alias apt="sudo apt"
+
+
+alias ga="git add"
+alias gc="git commit -m"
+alias gp="git push"
+alias gl="git pull"
+alias gqp='git add . && git commit -m "quick commit" && git push'
+alias gqc='git add . && git commit -m "quick commit"'
+alias push-vim="gqc && gp"
+
+alias film="~/Documents/Vozflix.AppImage"
+
+alias copy-vim="cp -r ~/.config/fish/ ~/vim/.config/"
+
+alias move-vim-code="cd ~/vim && push-vim"
+alias backup-alias="cp ~/.config/fish/config.fish ~/vim/.config/fish/"
+alias backup-vimrc="cp ~/.vimrc ~/vim/"
+alias backup-vimrcplug="cp ~/.vimrc.plug ~/vim/"
+alias backup-coc="cp ~/.vim/coc-settings.json ~/vim/.vim/"
+alias backup-history="history > ~/vim/history.txt"
+alias bneovim="cp ~/.vimrc ~/.config/nvim/init.vim"
+alias bcnvim="bneovim && cp ~/.config/nvim/init.vim ~/vim/.config/nvim/"
+
+alias bcvm="backup-alias && backup-vimrc && backup-vimrcplug && backup-coc"
+alias bc1="bcvm && backup-history && copy-vim"
+alias backup-vim="bc1 && move-vim-code && bcnvim"
+
+
+alias mfile='xdg-open .'
---
+++ .config/fish/environment.fish
@@ -0,0 +1,2 @@
+set -x EMAIL hyquaq@gmail.com
+set -x NAME Huy Nguyen
